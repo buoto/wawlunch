6fcec4bce58a75c3d9182ef204b8c8542bec8ca3
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-03-18 16:52:27 +0100

    [Android] NFC Switching screens

--- android/app/src/main/java/kantoniak/com/wawlunch/NfcActivity.java
+++ android/app/src/main/java/kantoniak/com/wawlunch/NfcActivity.java
@@ -11,7 +11,11 @@ import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.Profile;
 
 import java.io.IOException;
 
@@ -20,17 +24,49 @@ import kantoniak.com.wawlunch.nfc.NfcUtil;
 public class NfcActivity extends Activity {
 
     private static final String TAG = NfcActivity.class.getSimpleName();
+    private static int toWrite = 1;
+
+    private View mNotLoggedInView;
+    private View mProgressView;
+    private View mDoneView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+
+        mNotLoggedInView = findViewById(R.id.nfc_not_logged_in);
+        mProgressView = findViewById(R.id.nfc_getting_order);
+        mDoneView = findViewById(R.id.nfc_done);
+
         writeTag(getIntent());
+    }
+
+    private void showScreen(int i) {
+        mNotLoggedInView.setVisibility(View.GONE);
+        mProgressView.setVisibility(View.GONE);
+        mDoneView.setVisibility(View.GONE);
+
+        switch (i) {
+            case 0: mNotLoggedInView.setVisibility(View.VISIBLE); return;
+            case 1: mProgressView.setVisibility(View.VISIBLE); return;
+            case 2: mDoneView.setVisibility(View.VISIBLE); return;
+        }
+    }
+
+    private void handleTable(int table) {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+
+        if (token == null) {
+            showScreen(0);
+        }
+        showScreen(1);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         writeTag(intent);
+
     }
 
     private void writeTag(Intent intent) {
@@ -50,8 +86,9 @@ public class NfcActivity extends Activity {
 
             Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 
-            if (NfcUtil.writeTag(42, detectedTag)) {
+            if (NfcUtil.writeTag(NfcActivity.toWrite, detectedTag)) {
                 Log.i(TAG, "OK");
+                NfcActivity.toWrite++;
             } else {
                 Log.e(TAG, "NIEOK");
             }
@@ -63,6 +100,7 @@ public class NfcActivity extends Activity {
             Integer tableId = NfcUtil.readId(intent);
             if (tableId != null) {
                 Log.i(TAG, "TABLE ID " + tableId);
+                handleTable(tableId);
             }
         }
     }
