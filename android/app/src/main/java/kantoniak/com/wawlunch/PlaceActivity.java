4d55df4d2791e37992678796b97ebb9f0f68d148
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-03-18 09:24:55 +0100

    [Android] Set place title in ACtionBar

--- android/app/src/main/java/kantoniak/com/wawlunch/PlaceActivity.java
+++ android/app/src/main/java/kantoniak/com/wawlunch/PlaceActivity.java
@@ -2,14 +2,24 @@ package kantoniak.com.wawlunch;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PlaceActivity extends Activity {
+
+    private int placeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
 
+        Intent intent = getIntent();
+        if (intent == null) {
+            return;
+        }
+
+        setTitle(intent.getStringExtra(ShowPlaceIntent.PLACE_NAME));
+        placeId = intent.getIntExtra(ShowPlaceIntent.PLACE_ID, 0);
     }
 }
